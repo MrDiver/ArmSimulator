36bf6688e3949bc5ca8382921a51c9425500dc4f
MrDiver <mrdiverlp@gmail.com>
2020-04-17 13:26:19 +0200

    Some changes

---
+++ src/armparser/walker/CommandVisitor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Prodigy on 17.04.2020.
+//
+
+#include "CommandVisitor.h"
---
+++ src/armparser/walker/CommandVisitor.h
@@ -0,0 +1,16 @@
+//
+// Created by Prodigy on 17.04.2020.
+//
+
+#ifndef SRC_COMMANDVISITOR_H
+#define SRC_COMMANDVISITOR_H
+
+#include "ARMParserBaseVisitor.h"
+
+class CommandVisitor
+{
+public:
+    CommandVisitor();
+};
+
+#endif //SRC_COMMANDVISITOR_H
